16995a0492ce02782f33a79463dcc9fce82d31d3
Ardian Eka Candra <ardianoption@gmail.com>
2023-02-25 19:40:39 +0700

    delete static example

--- packages/static/src/utils/trpc.ts
+++
@@ -1,5 +0,0 @@
-// utils/trpc.ts
-import { createTRPCReact } from "@trpc/react-query";
-import type { MyRouter } from "@personal-branding/functions/trpc/utils";
-
-export const trpcMulti = createTRPCReact<MyRouter>();
